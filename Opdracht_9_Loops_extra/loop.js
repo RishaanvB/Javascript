c19ec19bee7178106869591d14af7510884ffb49
RishaanvB <69298949+RishaanvB@users.noreply.github.com>
2020-09-11 23:18:10 +0200

    Add files via upload

---
+++ Opdracht_9_Loops_extra/loop.js
@@ -0,0 +1,22 @@
+// Deel 1 
+let colors = ['yellow', 'blue', 'red', 'orange'];
+
+
+let i = 0;
+
+while (i < colors.length) {
+    
+    console.log(colors[i]);
+    i++;
+
+}
+
+// Deel 2
+
+let colorArray = ['yellow', 'blue', 'red', 'orange'];
+
+colorArray.forEach(color => console.log(' deel 2', color))
+
+
+
+
