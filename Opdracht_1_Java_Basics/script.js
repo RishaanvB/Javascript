2a1efe24e44942cc95e34a00051e5bfea213db05
Rishaan van Beveren <rishaanvanbeveren@gmail.com>
2020-09-17 00:05:44 +0200

    test commit script.js file in opdracht 1 map

--- Opdracht_1_Java_Basics/script.js
+++ Opdracht_1_Java_Basics/script.js
@@ -1,4 +1,4 @@
-console.log("Hello Winc Academy");
+//console.log("Hello Winc Academy");
 let name = "Rishaan";
 console.log(name);
 console.log("4 + 4");
@@ -6,7 +6,7 @@ let math = 10 + 7;
 let math2 = 9 + 3 * 6;
 let mathCombine = math2 + 3;
 let mathSuperCombine = (math2 * math) / 2;
-console.log('dit moet 17 zijn',math);
+console.log("dit moet 17 zijn", math);
 console.log(math2);
 console.log(mathCombine);
 console.log(math2 * math);
@@ -17,3 +17,4 @@ console.log(10 % 4);
 console.log(5 ** 3);
 let age = 33;
 console.log(typeof age);
+// test edit commit
